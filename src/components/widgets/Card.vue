3963f06cb7714cfdfabf359e4b5e71d3019349e7
crrashh1542 <kotsuki@crrashh.com>
2023-08-22 18:14:21 +0800

    chore: 将card不同主题设置入单独文件
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- src/components/widgets/Card.vue
+++ src/components/widgets/Card.vue
@@ -12,10 +12,11 @@ export default {
 
 <style lang="less">
 @import url('../../assets/styles/global.less');
+@import url('../../assets/styles/theme.less');
 
 .card {
     margin: 8px 10px 15px 0;
-    padding: 10px 20px;
+    padding: 10px 15px 10px 20px;
     height: 85px;
     width: 32%;
     display: inline-block;
@@ -41,43 +42,5 @@ export default {
     }
 }
 
-.color-theme {
-    background-color: @wu-color-theme-lighter;
-    border: 1px solid @wu-color-theme;
-}
-.hover\:color-theme:hover {
-    box-shadow: 0 0 6px 1px @wu-color-theme;
-}
-
-.color-amber {
-    background-color: @wu-color-amber-lighter;
-    border: 1px solid @wu-color-amber;
-}
-.hover\:color-amber:hover {
-    box-shadow: 0 0 3px 1px @wu-color-amber;
-}
-
-.color-yellow {
-    background-color: @wu-color-yellow-lighter;
-    border: 1px solid @wu-color-yellow;
-}
-.hover\:color-yellow:hover {
-    box-shadow: 0 0 3px 1px @wu-color-yellow;
-}
-
-.color-green {
-    background-color: @wu-color-green-lighter;
-    border: 1px solid @wu-color-green;
-}
-.hover\:color-green:hover {
-    box-shadow: 0 0 3px 1px @wu-color-green;
-}
-
-.color-cyan {
-    background-color: @wu-color-cyan-lighter;
-    border: 1px solid @wu-color-cyan;
-}
-.hover\:color-cyan:hover {
-    box-shadow: 0 0 3px 1px @wu-color-cyan;
-}
+/* 不同设备端适配 */
 </style>
