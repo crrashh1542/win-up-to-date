95648a4ed816dd75d73f6070b22b67aa8ee9e199
crrashh1542 <kotsuki@crrashh.com>
2023-08-26 16:50:13 +0800

    feat: 添加备案信息
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,9 +12,10 @@ export default {
 
 <template>
     <div class="footer">
-        <p>Windows 为 Microsoft Corporation 的注册商标，本项目与 Microsoft Corporation 无关。项目涉及的字体和图片仅用作学习，版权归原公司所有。</p>
-        <p>本项目遵循 GPL-3.0 开源协议，仅作为展示和交流学习之用途。 <s>实时查询接口均来自 <a href="https://github.com/uup-dump">UUP dump</a>。</s></p>
-        <p>当前项目版本：v{{ version }} / (C) Copyright 2023 Kotsuki Crrashh. All Rights Reserved.</p>
+        <p>Windows 为 Microsoft Corporation 的注册商标，本项目与 Microsoft Corporation 无关。项目涉及的字体和图片仅用作学习，所用图标来自 <a href="https://www.iconfont.cn">iconfont</a>，版权归原公司所有。</p>
+        <p>本项目遵循 GPL-3.0 开源协议，仅作展示和交流学习用途。<s>实时查询接口均来自 <a href="https://github.com/uup-dump">UUP dump</a>。</s></p>
+        <p>(C) Copyright 2023 Kotsuki Crrashh.</p>
+        <p>项目版本：v{{ version }} / <a href="//beian.miit.gov.cn">蜀ICP备2022029657号-2</a></p>
     </div>
 </template>
 
@@ -30,7 +31,8 @@ export default {
     color: @wu-color-catalog;
 
     p {
-        line-height: 1.7;
+        line-height: 1.6;
+        margin: .9em 0;
     }
 }
 </style>
