55647e7f9652d2f67066a80e56a6032ded46ec49
crrashh1542 <kotsuki@crrashh.com>
2023-09-29 02:46:24 +0800

    revert: Vive ID搁置
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Versions from '../views/Versions.vue'
-import FeatureIds from '../views/FeatureIds.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: Versions },
-        { path: '/featureIds', component: FeatureIds }
     ]
 })
 
