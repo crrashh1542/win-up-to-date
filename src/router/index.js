61905ea2cab71dce41a72792f678b040b5a92b2f
crrashh1542 <kotsuki@crrashh.com>
2023-09-30 02:22:14 +0800

    feat: 添加关于页面
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Versions from '../views/Versions.vue'
+import About from '../views/About.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: Versions },
+        { path: '/about', component: About },
     ]
 })
 
