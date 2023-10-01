d5d72c0f9852f67faf3b725f0d403f3364af5778
crrashh1542 <kotsuki@crrashh.com>
2023-10-02 01:44:49 +0800

    fix: 修复卡片显示错位的问题
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -20,24 +20,4 @@ export default {
     line-height: 1.2;
     display: inline-block;
 }
-
-/* 不同设备端适配 */
-
-// SEC 1 ------ 两列
-@media screen and (min-width: 1150px) {
-    .card {
-        --card-width: 32%;
-    }
-}
-@media screen and (max-width: 1150px) {
-    .card {
-        --card-width: 48%;
-    }
-}
-// SEC 2 ------ 一列
-@media screen and (max-width: 620px) {
-    .card {
-        --card-width: 100%;
-    }
-}
 </style>
--- src/views/Versions.vue
+++ src/views/Versions.vue
@@ -135,4 +135,24 @@ export default {
         }
     }
 }
+
+/* 卡片对于不同设备端适配 */
+
+// SEC 1 ------ 两列
+@media screen and (min-width: 1150px) {
+    .card {
+        --card-width: 32%;
+    }
+}
+@media screen and (max-width: 1150px) {
+    .card {
+        --card-width: 48%;
+    }
+}
+// SEC 2 ------ 一列
+@media screen and (max-width: 620px) {
+    .card {
+        --card-width: 100%;
+    }
+}
 </style>
