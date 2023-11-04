491a0595a71ff9c78bc10f96dee6bee76798ac49
crrashh1542 <kotsuki@crrashh.com>
2023-11-05 00:37:34 +0800

    chore(parseInfo): 版本带后缀时不返回hash
    减少移动端视觉占用空间
    
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- src/scripts/parseInfo.js
+++ src/scripts/parseInfo.js
@@ -20,20 +20,29 @@ const buildNum = buildInfo.build // 构建次数
 
 // 构建时间处理
 const parseBuildTime = t => {
-    // 查看 /temp/buildInfo.js 可知，time 参数格式均为 xxxx-xx-xxTyy:yy+zz:zz
-    const date = t.slice(0, 10)
-    const time = t.slice(11, 19)
-    const offset = t.slice(19, 25)
-
-    if(offset === '+08:00') {
+    // 由 /temp/buildInfo.js 可知，time 参数格式均为 xxxx-xx-xxTyy:yy+zz:zz
+    const date = t.slice(0, 10) // 日期
+    const time = t.slice(11, 19) // 时间
+    const offset = t.slice(19, 25) // 时区偏移
+    if(offset === '+08:00') { // 设置时区
         return date + ' ' + time + ' CST'
     } else {
         return date + ' ' + time
     }
 }
 
-// 数据处理
-const pVersion = 'v' + packageVerNum + '@' + buildHash + ' (build ' + buildNum + ')' // 完整版本
+// 项目版本处理
+const parseVersion = v => {
+    // 如果版本带后缀，就不返回 hash，减少移动端视觉占用空间
+    if(v.indexOf('-') !== -1) {
+        return 'v' + packageVerNum + ' (build ' + buildNum + ')'
+    } else {
+        return 'v' + packageVerNum + '@' + buildHash + ' (build ' + buildNum + ')'
+    }
+}
+
+// 数据合并
+const pVersion = parseVersion(packageVerNum) // 完整版本
 const pTime = parseBuildTime(buildTime) // 构建时间
 const pRepo = packageRepo.split('https://github.com/')[1] // 项目名
 const pVue = 'v' + packageDeps['vue'].split('^')[1] // Vue 框架版本
