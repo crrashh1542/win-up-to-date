0da7158d04570b94e8c3370cfec54480e212f160
crrashh1542 <kotsuki@crrashh.com>
2023-12-31 00:01:47 +0800

    fix: 修复移动端加载动画和404意外换行
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- src/components/LoadAnim.vue
+++ src/components/LoadAnim.vue
@@ -8,8 +8,8 @@ export default {
 </script>
 
 <template>
-    <div class="load absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
+    <div class="load absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 w-full">
         <fluent-progress-ring class="m-auto w-16 h-16"></fluent-progress-ring>
-        <p class="text-2xl m-8">数据加载中，请稍候</p>
+        <p class="m-10 text-2xl text-center">数据加载中，请稍候</p>
     </div>
 </template>
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,12 +1,25 @@
 <script>
+import { provideFluentDesignSystem, fluentButton } from "@fluentui/web-components"
+provideFluentDesignSystem().register(fluentButton())
+
 export default {
-    name: 'OtherNotFound'
+    name: 'OtherNotFound',
+    methods: {
+        routerToHome() {
+            this.$router.push('/')
+        }
+    }
 }
 </script>
 
 <template>
-    <div class="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
+    <div class="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 w-full text-center">
         <h1 class="text-4xl font-bold">404 NOT Found</h1>
         <p class="text-xl">前面的区域，以后再来探索吧！</p>
+        <router-link to="/">
+            <fluent-button class="text-lg">
+                <span>回到主页</span>
+            </fluent-button>
+        </router-link>
     </div>
 </template>
