c2feb7dbbdf40fae14535f7c8d94f44fac97a151
crrashh1542 <1542@crrashh.com>
2025-10-02 15:01:12 +0800

    refactor: [component] 重构Code
    - 修复 Button 组件和图标显示
    - 简化复制代码逻辑
    - 字体使用 JetBrains Mono
    - 取消 scope 限制
    - 自定义是否自动换行、是否可复制内容
    
    Signed-off-by: crrashh1542 <1542@crrashh.com>

---
+++ src/components/widgets/Code.vue
@@ -0,0 +1,63 @@
+<script setup>
+/**
+ * 可复制代码组件，代码右侧有个小复制按钮
+ * 用法：<Code value="xxx" />
+ */
+
+// 引入库
+import Button from './Button.vue'
+import useClipboard from 'vue-clipboard3'
+import { Icon } from '@iconify/vue'
+
+const { toClipboard } = useClipboard()
+
+const props = defineProps({ value: String, isCopiable: Boolean, isBreakWord: Boolean })
+
+// 处理是否断词强制换行
+let breakWord = ''
+if(props.isBreakWord) { breakWord = 'break-word' }
+</script>
+
+<template>
+    <!-- 代码本体 -->
+    <code :class="breakWord">{{ value }}</code>
+
+    <!-- 复制按钮 -->
+    <Button @click="toClipboard(value)" v-if="isCopiable">
+        <div>
+            <Icon icon="fluent:copy-16-regular" width="16" height="16" />
+            复制</div>
+    </Button>
+
+    <!-- 复制完成的提示 -->
+    <!-- TODO -->
+</template>
+
+<style lang="less">
+@import url('@/styles/global.less');
+
+code {
+    background-color: #f3f3f3;
+    border: 1px solid #ddd;
+    padding: .2em .4em;
+    line-height: 1.5rem;
+    border-radius: 4px;
+    font-family: 'JetBrains Mono';
+    font-size: 15px;
+}
+code.break-word {
+    word-break: break-all;
+}
+
+.button {
+	font-size: 1rem;
+    line-height: 1.25rem;
+    padding: .4em .6em;
+    margin: .2em .4em;
+    div {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+    }
+}
+</style>
