f39a258c565363d17dea5297924d148a06e24409
crrashh1542 <1542@crrashh.com>
2025-10-13 17:24:26 +0800

    chore(utils): 简化信息展示流程
    移除了 showRepoInfo 中处理数据的逻辑，将处理数据的所有逻辑转移到
    parseRepoInfo，让其它代码只需要调用它即可。
    
    Signed-off-by: crrashh1542 <1542@crrashh.com>

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
-import showInfo from './utils/showRepoInfo'
+import './utils/showRepoInfo'
 import router from './router'
 import App from './App.vue'
 
 createApp(App).use(router).mount('#app')
-
-showInfo()
--- src/utils/parseRepoInfo.js
+++ src/utils/parseRepoInfo.js
@@ -2,27 +2,39 @@
 /**
  * 此脚本用于在处理本项目相关信息
  * @author crrashh1542
- * @version 1.7
+ * @version 2.0
  */
 
 import packageInfo from '/package.json'
 import buildInfo from '/scripts/buildInfo.json'
 
-// packageInfo 相关
-const packageVerNum = packageInfo.version // 项目版本号
-const packageRepo = packageInfo.repository.url.split('+')[1] // 项目地址
+// 项目信息相关
+const pkgVersion = 'v' + packageInfo.version + ' (build ' + buildInfo.build + ')' // 项目版本号
+const pkgRepo = packageInfo.repository.url.split('+')[1] // 项目地址
+const pkgRepoName = pkgRepo.split('https://github.com/')[1] // 项目名
 
-// buildInfo 相关
-const buildNum = buildInfo.build // 构建次数
-
-// 数据合并
-const pVersion = 'v' + packageVerNum + ' (build ' + buildNum + ')' // 完整版本
-const pRepoName = packageRepo.split('https://github.com/')[1] // 项目名
+// 获取 build tag
+const buildTag = (() => {
+    // 处理版本号和分支
+    const buildNum = buildInfo.build
+    const buildBranch = buildInfo.branch
+    // 处理构建时间
+    const fullTime = buildInfo.time
+    const [bYear, bMonth, bDay, bHour, bMin] = [
+        fullTime.substring(2, 4),
+        fullTime.substring(5, 7),
+        fullTime.substring(8, 10),
+        fullTime.substring(11, 13),
+        fullTime.substring(14, 16),
+    ]
+    const buildTime = bYear + bMonth + bDay + '-' + bHour + bMin
+    return buildNum + '.' + buildBranch + '.' + buildTime
+})()
 
 // 导出数据
-export default [
-    ['站点版本', 'fluent:search-24-regular', pVersion],
-    ['项目地址', 'fluent:code-24-regular', packageRepo, pRepoName],
+let aboutInfo = [
+    ['站点版本', 'fluent:search-24-regular', pkgVersion],
+    ['项目地址', 'fluent:code-24-regular', pkgRepo, pkgRepoName],
     [
         '交流群组',
         'fluent:chat-24-regular',
@@ -30,3 +42,4 @@ export default [
         '442133970',
     ],
 ]
+export { aboutInfo, buildTag }
