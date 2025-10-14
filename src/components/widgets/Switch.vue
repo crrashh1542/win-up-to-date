3376165d6beb2d4cd131ff30533c2ecab72c6d13
crrashh1542 <1542@crrashh.com>
2025-10-14 22:15:44 +0800

    feat: 添加开关组件
    Signed-off-by: crrashh1542 <1542@crrashh.com>

---
+++ src/components/widgets/Switch.vue
@@ -0,0 +1,102 @@
+<!-- 
+    此组件移植自 FluentUI React v9 的 Switch 组件
+    参考：https://github.com/microsoft/fluentui/tree/@fluentui/react-components_v9.72.2/packages/react-components/react-switch/library/src/components/Switch
+-->
+<script setup>
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+    <div class="switch">
+        <input class="input-box" role="switch" type="checkbox" />
+        <div aria-hidden="true" class="indicator">
+            <Icon icon="fluent:circle-24-filled" />
+        </div>
+    </div>
+</template>
+
+<style lang="less">
+@import url('@/styles/global.less');
+
+.switch {
+    align-items: flex-start;
+    box-sizing: border-box;
+    display: inline-flex;
+    position: relative;
+
+    .input-box {
+        box-sizing: border-box;
+        cursor: pointer;
+        height: 100%;
+        margin: 0;
+        opacity: 0;
+        position: absolute;
+        width: 56px; // 40 + 2*8
+    }
+
+    .indicator {
+        border-radius: 10000px;
+        border: 1px solid #000;
+        line-height: 0;
+        box-sizing: border-box;
+        fill: currentColor;
+        flex-shrink: 0;
+        font-size: 18px;
+        height: 20px;
+        margin: 8px;
+        pointer-events: none;
+        transition-duration: 200ms;
+        transition-timing-function: cubic-bezier(0.33, 0, 0.67, 1);
+        transition-property: background, border, color;
+        width: 40px;
+        * {
+            transition-duration: 200ms;
+            transition-timing-function: cubic-bezier(0.33, 0 ,0.67, 1);
+            transition-property: transform;
+        }
+    }
+
+    .input-box:disabled {
+        cursor: default;
+    }
+    .input-box:checked ~ .indicator > * {
+        transform: translateX(20px);
+    }
+    .input-box:disabled ~ .indicator {
+        color: #5c5c5c;
+    }
+    .input-box:enabled:not(:checked) ~ .indicator {
+        color: #616161;
+        border-color: #616161;
+    }
+    .input-box:enabled:not(:checked):hover ~ .indicator {
+        color: #575757;
+        border-color: #575757;
+    }
+    .input-box:enabled:not(:checked):hover:active ~ .indicator {
+        color: #b3b3b3;
+        border-color: #b3b3b3;
+    }
+    .input-box:enabled:checked ~ .indicator {
+        background-color: #0078d4;
+        color: #fff;
+        border-color: transparent;
+    }
+    .input-box:enabled:checked:hover ~ .indicator {
+        background-color: #106ebe;
+        border-color: transparent;
+    }
+    .input-box:enabled:checked:hover:active ~ .indicator {
+        background-color: #005a9e;
+        border-color: transparent;
+    }
+    .input-box:disabled:not(:checked) ~ .indicator {
+        border-color: #e0e0e0;
+    }
+    .input-box:disabled:checked ~ .indicator {
+        background-color: #f0f0f0;
+        border-color: transparent;
+    }
+}
+
+</style>
