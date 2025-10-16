8676922ba0b66c62802d94244c8953840b3d0edb
crrashh1542 <1542@crrashh.com>
2025-10-16 14:31:43 +0800

    feat: 新增设置
    通过 pinia 实现设置数据的跨组件存储。
    
    Signed-off-by: crrashh1542 <1542@crrashh.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
-import './utils/showRepoInfo'
+import { createPinia } from 'pinia'
 import router from './router'
-import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+import App from './App.vue'
+import './utils/showRepoInfo'
+
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(router)
+app.use(pinia)
+app.mount('#app')
