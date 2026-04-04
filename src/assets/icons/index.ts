1ee213f6977dddd880ec283a92f42e31065fb1f9
crrashh1542 <1542@crrashh.com>
2026-04-04 10:40:46 +0800

    fix(icon): 修复amicon不到处svg的问题
    amicon v1.0.0-alpha.9 现在只导出 svg 字符串，无法直接使用，需处理。
    
    Signed-off-by: crrashh1542 <1542@crrashh.com>

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,22 +1,21 @@
 // 此文件用于中转图标文件，图标均来自 amicons 与 iconfont。
 // 本目录下图标来自 iconfont，见 https://www.iconfont.cn/manage/index?manage_type=myprojects&projectId=4217937
 
-import azure from '@studio384/amicons/icons/azure.svg'
+import { aiAzure, aiCompactDisc, aiServer, aiWindows } from '@studio384/amicons'
 import branch from './branch.svg'
-import iso from '@studio384/amicons/icons/compact-disc.svg'
 import rocket from './rocket.svg'
-import server from '@studio384/amicons/icons/server.svg'
 import windows10 from './windows10.svg'
-import windows11 from '@studio384/amicons/icons/windows.svg'
+
+const toSvgDataUri = (svg: string) => `data:image/svg+xml,${encodeURIComponent(svg)}`
 
 const icons = {
-    azure,
+    azure: toSvgDataUri(aiAzure.data),
     branch,
-    iso,
-    server,
+    iso: toSvgDataUri(aiCompactDisc.data),
+    server: toSvgDataUri(aiServer.data),
     rocket,
     windows10,
-    windows11
+    windows11: toSvgDataUri(aiWindows.data)
 }
 
 export default icons
