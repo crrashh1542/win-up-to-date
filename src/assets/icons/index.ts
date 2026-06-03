299136627d0dfdda3d36f4ffc9c6e92a4e15f20a
crrashh1542 <1542@crrashh.com>
2026-06-03 17:59:47 +0800

    chore(icon): 移除amicon
    会受到 AGPL-3.0 许可证影响，故继续使用 iconfont 提供的 svg
    
    Signed-off-by: crrashh1542 <1542@crrashh.com>

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,21 +1,19 @@
-// 此文件用于中转图标文件，图标均来自 amicons 与 iconfont。
-// 本目录下图标来自 iconfont，见 https://www.iconfont.cn/manage/index?manage_type=myprojects&projectId=4217937
+// 此文件用于中转图标文件，本目录下图标来自 iconfont，见 https://www.iconfont.cn/manage/index?manage_type=myprojects&projectId=4217937
 
-import { aiAzure, aiCompactDisc, aiServer, aiWindows } from '@studio384/amicons'
-import branch from './branch.svg'
-import rocket from './rocket.svg'
+import azure from './azure.svg'
+import insider from './insider.svg'
+import iso from './disc.svg'
+import server from './server.svg'
 import windows10 from './windows10.svg'
-
-const toUri = (svg: string) => `data:image/svg+xml,${encodeURIComponent(svg)}`
+import windows11 from './windows11.svg'
 
 const icons: Record<string, string> = {
-    azure: toUri(aiAzure.data),
-    branch,
-    iso: toUri(aiCompactDisc.data),
-    server: toUri(aiServer.data),
-    rocket,
+    azure,
+    iso,
+    insider,
+    server,
     windows10,
-    windows11: toUri(aiWindows.data)
+    windows11
 }
 
 export default icons
