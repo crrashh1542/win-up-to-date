b2d0a8e8804ab42932cd89bcef301ca9a8b5945e
crrashh1542 <1542@crrashh.com>
2026-07-09 14:15:17 +0800

    chore(lint): 修复vue文件报错的问题
    此前只配置了 eslint-plugin-vue，没有配置 vue-eslint-parser
    
    Signed-off-by: crrashh1542 <1542@crrashh.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import vue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
 import tsParser from '@typescript-eslint/parser'
 import tsPlugin from '@typescript-eslint/eslint-plugin'
 
@@ -14,20 +15,35 @@ export default [
         languageOptions: {
             ecmaVersion: 'latest',
             sourceType: 'module',
-        }
+        },
     },
     {
-        files: ['**/*.{ts,vue}'],
+        files: ['**/*.ts'],
         languageOptions: {
             parser: tsParser,
-            ecmaVersion: 'latest',
-            sourceType: 'module',
         },
         plugins: {
             '@typescript-eslint': tsPlugin,
         },
         rules: {
             ...tsPlugin.configs.recommended.rules,
+            'no-undef': 'off',
+        },
+    },
+    {
+        files: ['**/*.vue'],
+        languageOptions: {
+            parser: vueParser,
+            parserOptions: {
+                parser: tsParser,
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+        },
+        rules: {
+            ...tsPlugin.configs.recommended.rules,
+            'no-undef': 'off',
         },
     },
 ]
